70dc8b7870d27ace19d9e45ebac990e381f71dde
DaviSFS21 <davisfs2110@gmail.com>
2025-04-03 22:18:13 -0300

    refactor: change logic used in the resolution

--- queue.c
+++ queue.c
@@ -6,58 +6,58 @@ typedef struct cel {
     struct cel *next;
 } celula;
 
-celula *createListFromArray(celula **list, int x[]) {
-  celula *newElement = malloc(sizeof(celula)); // allocate memory for the new element
-  newElement -> value = x[0];
-  newElement -> next = NULL;
-  *list = newElement; // saving list's first element
-  celula *lastCreated = newElement; // helper variable
-
-  int i;
-  for (i = 1; i < 5; i++) {
-      newElement = malloc(sizeof(celula));
-      newElement -> value = x[i];
-      newElement -> next = NULL;
-      lastCreated -> next = newElement;
-      lastCreated = newElement; // keeping track of the last element
-  }
-
-  return lastCreated; // the last element created might be the end of the queue
+void printCelula(celula *start) { // prints all elements
+    if (start == NULL) return;
+    printf("%i\n", start -> value);
+    printCelula(start -> next);
 }
 
-void printCelula(celula *list) { // prints all elements
-    if (list == NULL) return;
-    printf("%i\n", list -> value);
-    printCelula(list -> next);
-}
-
-void addElement(celula *end, int x) { // add element at the beginning
+void addElement(celula **start, celula **end, int x) { // add element at the beginning
   celula *new = malloc(sizeof(celula));
-  end -> next = new;
   new -> value = x;
   new -> next = NULL;
+
+  if(*start == NULL && *end == NULL) {
+    *start = new;
+    *end = new;
+  } else {
+    (*end) -> next = new;
+    *end = new;
+  }
 }
 
-int removeElement(celula **list) {
-  celula *lixo = *list;
-  int x = lixo -> value;
-  *list = lixo -> next;
-  free(lixo);
-  return x;
+int removeElement(celula **start, celula **end) {
+  if(*start == NULL && *end == NULL) return -9999;
+  else {
+    celula *del = *start; // del var stores the first cell
+    int x = del -> value;
+    if (del -> next == NULL) { // if there is only one element, both list adresses will be NULL
+      *start = NULL;
+      *end = NULL;
+    } else { // otherwise, start will get the element after the deleted element
+      *start = del -> next;
+    }
+    free(del);
+    return x;
+  }
 }
 
 int main() {
   int intArray[] = {1,2,3,4,5};
-  celula *listStart;
-  celula *listEnd = createListFromArray(&listStart, intArray);
+  celula *listStart = NULL;
+  celula *listEnd = NULL;
+
+  for(int i = 0; i < 5; i++) {
+    addElement(&listStart, &listEnd, intArray[i]);
+  }
   
   printCelula(listStart);
   printf("\n-------------------------------------------\n");
-  addElement(listEnd,6);
+  addElement(&listStart, &listEnd, 6);
   printf("List with added element: \n");
   printCelula(listStart);
   printf("\n-------------------------------------------\n");
-  printf("Removed at start: %i", removeElement(&listStart));
+  printf("Removed at start: %i", removeElement(&listStart, &listEnd));
   printf("\n-------------------------------------------\n");
   printCelula(listStart);
   printf("\n-------------------------------------------\n");
