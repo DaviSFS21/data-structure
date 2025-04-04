d1ef5ae1e6573afefece4a6d421600d0242c7beb
DaviSFS21 <davisfs2110@gmail.com>
2025-04-04 12:15:30 -0300

    refactor: improve list management and element handling in stack.c

--- stack.c
+++ stack.c
@@ -6,71 +6,59 @@ typedef struct cel {
     struct cel *next;
 } celula;
 
-celula *createListFromArray(int x[]) { // headed lists allows us to treat every cell with a same treatment
-  celula *newElement = malloc(sizeof(celula)); // allocate memory for the new element
-  newElement -> value = 0;
-  newElement -> next = NULL;
-  celula *list = newElement; // saving list's first element
-  celula *lastCreated = newElement; // helper variable
-
-  int i;
-  for (i = 0; i < 5; i++) {
-      newElement = malloc(sizeof(celula));
-      newElement -> value = x[i];
-      newElement -> next = NULL;
-      lastCreated -> next = newElement;
-      lastCreated = newElement; // keeping track of the last element
-  }
-
-  return list;
-}
-
 void printCelula(celula *list) { // prints all elements
-    if (list == NULL) return;
-    printf("%i\n", list -> value);
-    printCelula(list -> next);
+  if (list == NULL) {
+    printf("\n");
+    return;
+  }
+  printf("%i, ", list -> value);
+  printCelula(list -> next);
 }
 
-void addElement(celula *p, int x) { // add element after the given address
+void addElement(celula *list, int x) { // add element after the given address
   celula *new = malloc(sizeof(celula));
-  new -> next = p -> next;
+  new -> next = list -> next;
   new -> value = x;
-  p -> next = new;
+  list -> next = new;
 }
 
-int removeElement(celula *p) { // remove the element after the given address
-  if(!(p -> next == NULL)) {
-    celula *lixo = p -> next;
-    int x = lixo -> value;
-    p -> next = lixo -> next;
-    free(lixo);
-    return x;
-  } else {
-    return -999;
-  }
-}
-
-celula *getElement(celula *list, int pos) { // get a random cel for operations
-  pos++;
-  celula *current = list;
-  for(int i = 1; i < pos; i++) {
-    current = current -> next;
-  }
-  return current;
+int removeElement(celula *list) { // remove the element after the given address
+  if(list -> next == NULL) return -9999;
+  celula *lixo = list -> next;
+  int x = lixo -> value;
+  list -> next = lixo -> next;
+  free(lixo);
+  return x;
 }
 
 int main() {
   int intArray[] = {1,2,3,4,5};
-  celula *list = createListFromArray(intArray);  
+  celula *list = malloc(sizeof(celula));
+  list -> next = NULL;
   
-  addElement(list,0);
+  for(int i = 0; i < 5; i++) {
+    addElement(list, intArray[i]);
+  }
+
   printCelula(list -> next);
   printf("\n-------------------------------------------\n");
-  printf("Removed element: %i", removeElement(list));
-  printf("\n-------------------------------------------\n");
-  printf("Removed element: %i", removeElement(list));
-  printf("\n-------------------------------------------\n");
+  addElement(list, 6);
+  printf("List with added element: 6\n");
   printCelula(list -> next);
+  printf("-------------------------------------------\n");
+  addElement(list, 7);
+  printf("List with added element: 7\n");
+  printCelula(list -> next);
+  printf("-------------------------------------------\n");
+  printf("Removed: %i\n", removeElement(list));
+  printCelula(list -> next);
+  printf("-------------------------------------------\n");
+  printf("Removed: %i\n", removeElement(list));
+  printCelula(list -> next);
+  printf("-------------------------------------------\n");
+  printf("Removed: %i\n", removeElement(list));
+  printCelula(list -> next);
+  printf("-------------------------------------------\n");
 
   return 0;
 }
