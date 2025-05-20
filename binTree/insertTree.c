e1e34223223349f270cc1127abdfec1cdbeaa1e9
DaviSFS21 <davisfs2110@gmail.com>
2025-05-19 21:04:19 -0300

    feat: insertTree algorithm

---
+++ binTree/insertTree.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct cel {
+  int valor;
+  struct cel *esq;
+  struct cel *dir;
+} no;
+
+void printTree(no *bt) {
+  if (bt -> esq != NULL)
+    printTree(bt -> esq);
+  printf("%i", bt -> valor);
+  if (bt -> esq != NULL)
+    printTree(bt -> dir);
+}
+
+void insertTree(no **bt, int x) {
+  no *new = malloc(sizeof(no));
+  new -> valor = x;
+  new -> esq = NULL;
+  new -> dir = NULL;
+  if (*bt == NULL) {
+    *bt = new;
+    return;
+  }
+  if (x < (*bt) -> valor) insertTree(&(*bt) -> esq, x);
+  if (x > (*bt) -> valor) insertTree(&(*bt) -> dir, x);
+}
+
+int main() {
+  no *tree = NULL;
+
+  insertTree(&tree, 4);
+  insertTree(&tree, 2);
+  insertTree(&tree, 10);
+  insertTree(&tree, 5);
+  insertTree(&tree, 7);
+
+  printTree(tree);
+
+  return 0;
+}
